b6d6ba16e6f5aa0163e5c496d03d992f913aa78e
Danish Abdullah <dev@danishabdullah.com>
2015-01-04 14:49:38 +0100

    added more important stuff

--- .tmuxp/env.sh
+++ .tmuxp/env.sh
@@ -1,3 +1,5 @@
-source /home/danish/anaconda/bin/activate web_frontend
+#! /bin/bash
+source /Users/danish/anaconda/bin/activate webfrontend
+export FLASK_ENV=DEBUG
 export DB_PORT=tcp://localhost:5432 
 export RABBITMQ_PORT=tcp://localhost:5672
